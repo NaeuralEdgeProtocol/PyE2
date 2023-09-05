4734460548382466e06f56f67c43411b076b85e7
Stefan Saraev <stefan.saraev@yahoo.com>
2023-09-05 14:30:49 +0300

    fix(io_formatter): mixin imported from libraries if available

--- PyE2/io_formatter/mixins/__init__.py
+++ PyE2/io_formatter/mixins/__init__.py
@@ -1,1 +1,6 @@
-from .plugins_manager_mixin import _PluginsManagerMixin
+try:
+  from libraries import _PluginsManagerMixin
+except:
+  # we run this without libraries
+  from .plugins_manager_mixin import _PluginsManagerMixin
+  pass
