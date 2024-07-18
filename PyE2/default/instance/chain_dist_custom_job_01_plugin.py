01631a42bba3601e88d75154be1c976654e76b50
Stefan Saraev <stefan.saraev@yahoo.com>
2024-07-18 16:49:56 +0300

    style: add some indents

--- PyE2/default/instance/chain_dist_custom_job_01_plugin.py
+++ PyE2/default/instance/chain_dist_custom_job_01_plugin.py
@@ -4,33 +4,30 @@ from ...base import Instance
 class ChainDistCustomJob01(Instance):
   signature = "PROCESS_REAL_TIME_COLLECTED_DATA_CUSTOM_EXEC_CHAIN_DIST"
 
-  def convert_to_base64(self, callable_code: callable):
-    return self.pipeline._get_base64_code(callable_code)
-
   def add_custom_code_callbacks(
-    self,
-    main_node_process_real_time_collected_data: callable,
-    main_node_finish_condition: callable,
-    main_node_finish_condition_kwargs: dict,
-    main_node_aggregate_collected_data: callable,
-    worker_node_code: callable,
-  ):
+      self,
+      main_node_process_real_time_collected_data: callable,
+      main_node_finish_condition: callable,
+      main_node_finish_condition_kwargs: dict,
+      main_node_aggregate_collected_data: callable,
+      worker_node_code: callable,
+    ):
     self.update_instance_config(
       config={
-        "CUSTOM_CODE_PROCESS_REAL_TIME_COLLECTED_DATA": self.convert_to_base64(main_node_process_real_time_collected_data),
-        "CUSTOM_CODE_FINISH_CONDITION": self.convert_to_base64(main_node_finish_condition),
-        "CUSTOM_CODE_AGGREGATE_COLLECTED_DATA": self.convert_to_base64(main_node_aggregate_collected_data),
-        "CUSTOM_CODE_REMOTE_NODE": self.convert_to_base64(worker_node_code),
+        "CUSTOM_CODE_PROCESS_REAL_TIME_COLLECTED_DATA": self.pipeline._get_base64_code(main_node_process_real_time_collected_data),
+        "CUSTOM_CODE_FINISH_CONDITION": self.pipeline._get_base64_code(main_node_finish_condition),
+        "CUSTOM_CODE_AGGREGATE_COLLECTED_DATA": self.pipeline._get_base64_code(main_node_aggregate_collected_data),
+        "CUSTOM_CODE_REMOTE_NODE": self.pipeline._get_base64_code(worker_node_code),
         "FINISH_CONDITION_KWARGS": main_node_finish_condition_kwargs
       }
     )
     return
 
   def add_worker_node_configuration(
-    self,
-    worker_node_pipeline_config: dict,
-    worker_node_plugin_config: dict,
-  ):
+      self,
+      worker_node_pipeline_config: dict,
+      worker_node_plugin_config: dict,
+    ):
     self.update_instance_config(
       config={
         "NODE_PIPELINE_CONFIG": worker_node_pipeline_config,
@@ -40,12 +37,12 @@ class ChainDistCustomJob01(Instance):
     return
 
   def add_main_node_configuration(
-    self,
-    nr_worker_nodes: int,
-    specific_worker_nodes: list = None,
-    worker_node_timeout: int = 150,
-    cancel_all_jobs_on_exception: bool = False,
-  ):
+      self,
+      nr_worker_nodes: int,
+      specific_worker_nodes: list = None,
+      worker_node_timeout: int = 150,
+      cancel_all_jobs_on_exception: bool = False,
+    ):
     self.update_instance_config(
       config={
         "NR_REMOTE_NODES": nr_worker_nodes,
