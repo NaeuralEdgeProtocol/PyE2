bffa60bb34d88fd9ff8c3d15288182cfa76d4658
Cristi Bleotiu <cristibleotiu@gmail.com>
2024-08-09 10:22:40 +0300

    fix: added local node to allowed nodes in case the session address is also the address of a node

--- PyE2/_ver.py
+++ PyE2/_ver.py
@@ -1,4 +1,4 @@
-__VER__ = "1.1.0"
+__VER__ = "1.1.1"
 
 if __name__ == "__main__":
   with open("pyproject.toml", "rt") as fd:
--- PyE2/base/generic_session.py
+++ PyE2/base/generic_session.py
@@ -345,7 +345,7 @@ class GenericSession(BaseDecentrAIObject):
       node_whitelist = dict_msg.get(HB.EE_WHITELIST, [])
       node_secured = dict_msg.get(HB.SECURED, False)
 
-      self._dct_can_send_to_node[node_addr] = not node_secured or self.bc_engine.address_no_prefix in node_whitelist
+      self._dct_can_send_to_node[node_addr] = not node_secured or self.bc_engine.address_no_prefix in node_whitelist or self.bc_engine.address == node_addr
       return
 
     def __on_heartbeat(self, dict_msg: dict, msg_node_addr, msg_pipeline, msg_signature, msg_instance):
